48bcc62633cc2fc3843009e2270a8f83e819e537
jssteinberg <jssteinberg@pm.me>
2021-03-02 12:46:44 +0100

    Fix away rel='prefetch' to new correct sapper:prefetch

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -7,8 +7,8 @@
 		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href="/">Front</a></li>
 		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="/about">About</a></li>
 
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+		<!-- for the blog link, we're using sapper:prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-			  <li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="/blog">Blog</a></li>
+			  <li><a sapper:prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="/blog">Blog</a></li>
 	</ul>
 </nav>
